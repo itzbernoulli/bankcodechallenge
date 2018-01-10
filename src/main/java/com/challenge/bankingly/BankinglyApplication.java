5474eab1d91119ec94cf852aa67f9ae948374be0
oyinloye-ai <ayodeji.oyinloye@meltwater.org>
2018-01-10 04:02:25 +0200

    Initial Commit

---
+++ src/main/java/com/challenge/bankingly/BankinglyApplication.java
@@ -0,0 +1,12 @@
+package com.challenge.bankingly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankinglyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankinglyApplication.class, args);
+	}
+}
